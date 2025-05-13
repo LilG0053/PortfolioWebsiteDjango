c98eeb43ba530fa7b65b943f7e5b42cda998d868
LilG0053 <ekimmel8@gatech.edu>
2025-05-13 18:23:54 -0400

    Added resume

--- PortfolioWebsiteNew/settings.py
+++ PortfolioWebsiteNew/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'projects',
     'skills',
     'home',
+    'PortfolioWebsiteNew',
 ]
 
 MIDDLEWARE = [
--- PortfolioWebsiteNew/urls.py
+++ PortfolioWebsiteNew/urls.py
@@ -18,12 +18,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('projects/', include('projects.urls')),
     path('skills/', include('skills.urls')),
     path('', include('home.urls')),
+    path('resume/', views.resume_page, name='resume'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
